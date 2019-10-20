a55ba08331d63f8b9b4cda7c16cb101a7644ef0d
Jamie Terry <jnterry@ntlworld.com>
2019-10-20 17:26:22 +0100

    Include character position infomation in tokenizer

--- lib/tokenizer.ts
+++ lib/tokenizer.ts
@@ -43,6 +43,16 @@ interface Token {
 	 * as per the USFM spec: https://ubsicap.github.io/usfm/about/syntax.html#newlines
 	 */
 	value      : string;
+
+	/**
+	 * Index of the first character in the token
+	 */
+	min: number,
+
+	/**
+	 * Index of the last character in the token
+	 */
+	max: number,
 };
 
 /**
@@ -50,8 +60,9 @@ interface Token {
  */
 export function* tokenizer(text: string) : IterableIterator<Token> {
 
-	let cur_value = "";
-	let cur_kind : TokenType | undefined = undefined;
+	let cur_value : string = "";
+	let cur_start : number = 0;
+	let cur_kind  : TokenType | undefined = undefined;
 
 	let i = 0;
 	while(i < text.length){
@@ -60,6 +71,7 @@ export function* tokenizer(text: string) : IterableIterator<Token> {
 		switch(cur_kind){
 			case undefined:
 				cur_value = c;
+				cur_start = i;
 				if(isWhitespace(c)){ cur_kind = TokenType.Whitespace; }
 				else if(c === '|' ){ cur_kind = TokenType.VBar;       }
 				else if(c === '\\'){ cur_kind = TokenType.Marker;     }
@@ -74,12 +86,12 @@ export function* tokenizer(text: string) : IterableIterator<Token> {
 					// CR, LF and CRLF should all be treated as LF (IE: \n)
 					// https://ubsicap.github.io/usfm/about/syntax.html?highlight=whitespace#newlines
 					cur_value = cur_value.replace(/\r\n?/g, "\n");
-					yield { kind: cur_kind, value: cur_value };
+					yield { kind: cur_kind, value: cur_value, min: cur_start, max: i-1 };
 					cur_kind = undefined;
 				}
 				break;
 			case TokenType.VBar:
-				yield { kind: cur_kind, value: cur_value };
+				yield { kind: cur_kind, value: cur_value, min: cur_start, max: i-1 };
 				cur_kind = undefined;
 				break;
 			case TokenType.Marker:
@@ -91,7 +103,7 @@ export function* tokenizer(text: string) : IterableIterator<Token> {
 						cur_value += '*';
 						++i;
 					}
-					yield { kind: cur_kind, value: cur_value };
+					yield { kind: cur_kind, value: cur_value, min: cur_start, max: i-1 };
 					cur_kind = undefined;
 				}
 				break;
@@ -100,7 +112,7 @@ export function* tokenizer(text: string) : IterableIterator<Token> {
 					cur_value += c;
 					++i;
 				} else {
-					yield { kind: cur_kind, value: cur_value };
+					yield { kind: cur_kind, value: cur_value, min: cur_start, max: i-1 };
 					cur_kind = undefined;
 				}
 				break;
@@ -111,7 +123,7 @@ export function* tokenizer(text: string) : IterableIterator<Token> {
 
 	if(cur_kind !== undefined){
 		cur_value = cur_value.replace(/\r\n?/g, "\n");
-		yield { kind: cur_kind, value: cur_value };
+		yield { kind: cur_kind, value: cur_value, min: cur_start, max: text.length-1 };
 	}
 }
 
--- test/tokenizer.js
+++ test/tokenizer.js
@@ -15,126 +15,131 @@ describe("tokenizer", () => {
   describe("Single tokens", () => {
     it('Whitespace', () => {
       expect(Array.from(tokenizer(' '))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: ' ' }
+        { kind: TokenType.Whitespace, value: ' ', min: 0, max: 0 }
       ]);
 
       expect(Array.from(tokenizer('\n'))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: '\n' }
+        { kind: TokenType.Whitespace, value: '\n', min: 0, max: 0 }
       ]);
 
       expect(Array.from(tokenizer('\r'))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: '\n' }
+        { kind: TokenType.Whitespace, value: '\n', min: 0, max: 0 }
       ]);
 
       expect(Array.from(tokenizer('\r\n'))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: '\n' }
+        { kind: TokenType.Whitespace, value: '\n', min: 0, max: 1 }
       ]);
 
       expect(Array.from(tokenizer('\t'))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: '\t' }
+        { kind: TokenType.Whitespace, value: '\t', min: 0, max: 0 }
       ]);
 
       expect(Array.from(tokenizer('\t  \t  '))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: '\t  \t  ' }
+        { kind: TokenType.Whitespace, value: '\t  \t  ', min: 0, max: 5 }
       ]);
 
       expect(Array.from(tokenizer('\r \t\n\t\r\n  \t'))).to.deep.equal([
-        { kind: TokenType.Whitespace, value: '\n \t\n\t\n  \t' }
+        { kind: TokenType.Whitespace, value: '\n \t\n\t\n  \t', min: 0, max: 9 }
       ]);
     });
 
     it('VBar', () => {
       expect(Array.from(tokenizer('|'))).to.deep.equal([
-        { kind: TokenType.VBar, value: '|' }
+        { kind: TokenType.VBar, value: '|', min: 0, max: 0 }
       ]);
     });
 
     it('Word', () => {
       expect(Array.from(tokenizer('a'))).to.deep.equal([
-        { kind: TokenType.Word, value: 'a' }
+        { kind: TokenType.Word, value: 'a', min: 0, max: 0 }
       ]);
 
       expect(Array.from(tokenizer('hello'))).to.deep.equal([
-        { kind: TokenType.Word, value: 'hello' }
+        { kind: TokenType.Word, value: 'hello', min: 0, max: 4 }
       ]);
 
       expect(Array.from(tokenizer('"hello"'))).to.deep.equal([
-        { kind: TokenType.Word, value: '"hello"' }
+        { kind: TokenType.Word, value: '"hello"', min: 0, max: 6 }
       ]);
 
       expect(Array.from(tokenizer('here-is-some-text123**!'))).to.deep.equal([
-        { kind: TokenType.Word, value: 'here-is-some-text123**!' }
+        { kind: TokenType.Word, value: 'here-is-some-text123**!', min: 0, max: 22 }
       ]);
     });
 
     it('Marker', () => {
-      expect(Array.from(tokenizer('\v'))).to.deep.equal([
-        { kind: TokenType.Word, value: '\v' }
+      expect(Array.from(tokenizer("\\v"))).to.deep.equal([
+        { kind: TokenType.Marker, value: "\\v", min: 0, max: 1 }
       ]);
 
-      expect(Array.from(tokenizer('\litl'))).to.deep.equal([
-        { kind: TokenType.Word, value: '\litl' }
+      expect(Array.from(tokenizer("\\litl"))).to.deep.equal([
+        { kind: TokenType.Marker, value: "\\litl", min: 0, max: 4 }
       ]);
 
-      expect(Array.from(tokenizer('\lik*'))).to.deep.equal([
-        { kind: TokenType.Word, value: '\lik*' }
+      expect(Array.from(tokenizer("\\lik*"))).to.deep.equal([
+        { kind: TokenType.Marker, value: "\\lik*", min: 0, max: 4 }
       ]);
     });
   });
 
   it("Multiple Tokens", () => {
     expect(Array.from(tokenizer('hello world'))).to.deep.equal([
-      { kind: TokenType.Word,       value: 'hello' },
-      { kind: TokenType.Whitespace, value: ' '     },
-      { kind: TokenType.Word,       value: 'world' },
+      { min: 0, max:  4, kind: TokenType.Word,       value: 'hello' },
+      { min: 5, max:  5, kind: TokenType.Whitespace, value: ' '     },
+      { min: 6, max: 10, kind: TokenType.Word,       value: 'world' },
     ]);
 
     expect(Array.from(tokenizer("\\v 1 In the beginning"))).to.deep.equal([
-      { kind: TokenType.Marker,     value: "\\v"       },
-      { kind: TokenType.Whitespace, value: " "         },
-      { kind: TokenType.Word,       value: "1"         },
-      { kind: TokenType.Whitespace, value: " "         },
-      { kind: TokenType.Word,       value: "In"        },
-      { kind: TokenType.Whitespace, value: " "         },
-      { kind: TokenType.Word,       value: "the"       },
-      { kind: TokenType.Whitespace, value: " "         },
-      { kind: TokenType.Word,       value: "beginning" },
+      { min:  0, max:  1, kind: TokenType.Marker,     value: "\\v"       },
+      { min:  2, max:  2, kind: TokenType.Whitespace, value: " "         },
+      { min:  3, max:  3, kind: TokenType.Word,       value: "1"         },
+      { min:  4, max:  4, kind: TokenType.Whitespace, value: " "         },
+      { min:  5, max:  6, kind: TokenType.Word,       value: "In"        },
+      { min:  7, max:  7, kind: TokenType.Whitespace, value: " "         },
+      { min:  8, max: 10, kind: TokenType.Word,       value: "the"       },
+      { min: 11, max: 11, kind: TokenType.Whitespace, value: " "         },
+      { min: 12, max: 20, kind: TokenType.Word,       value: "beginning" },
     ]);
 
     expect(Array.from(tokenizer("\\v 1\tIn  the\nbeginning"))).to.deep.equal([
-      { kind: TokenType.Marker,     value: "\\v"       },
-      { kind: TokenType.Whitespace, value: " "         },
-      { kind: TokenType.Word,       value: "1"         },
-      { kind: TokenType.Whitespace, value: "\t"        },
-      { kind: TokenType.Word,       value: "In"        },
-      { kind: TokenType.Whitespace, value: "  "        },
-      { kind: TokenType.Word,       value: "the"       },
-      { kind: TokenType.Whitespace, value: "\n"        },
-      { kind: TokenType.Word,       value: "beginning" },
+      { min:  0, max:  1, kind: TokenType.Marker,     value: "\\v"       },
+      { min:  2, max:  2, kind: TokenType.Whitespace, value: " "         },
+      { min:  3, max:  3, kind: TokenType.Word,       value: "1"         },
+      { min:  4, max:  4, kind: TokenType.Whitespace, value: "\t"        },
+      { min:  5, max:  6, kind: TokenType.Word,       value: "In"        },
+      { min:  7, max:  8, kind: TokenType.Whitespace, value: "  "        },
+      { min:  9, max: 11, kind: TokenType.Word,       value: "the"       },
+      { min: 12, max: 12, kind: TokenType.Whitespace, value: "\n"        },
+      { min: 13, max: 21, kind: TokenType.Word,       value: "beginning" },
     ]);
 
     expect(Array.from(tokenizer("\\v 1 In \\w the|lemma=\"test\"\\w*beginning"))).to.deep.equal([
-      { kind: TokenType.Marker,     value: "\\v"            },
-      { kind: TokenType.Whitespace, value: " "              },
-      { kind: TokenType.Word,       value: "1"              },
-      { kind: TokenType.Whitespace, value: " "              },
-      { kind: TokenType.Word,       value: "In"             },
-      { kind: TokenType.Whitespace, value: " "              },
-      { kind: TokenType.Marker,     value: "\\w"            },
-      { kind: TokenType.Whitespace, value: " "              },
-      { kind: TokenType.Word,       value: "the"            },
-      { kind: TokenType.VBar,       value: "|"              },
-      { kind: TokenType.Word,       value: "lemma=\"test\"" },
-      { kind: TokenType.Marker,     value: "\\w*"           },
-      { kind: TokenType.Word,       value: "beginning"      },
+      { min:  0, max:  1, kind: TokenType.Marker,     value: "\\v"            },
+      { min:  2, max:  2, kind: TokenType.Whitespace, value: " "              },
+      { min:  3, max:  3, kind: TokenType.Word,       value: "1"              },
+      { min:  4, max:  4, kind: TokenType.Whitespace, value: " "              },
+      { min:  5, max:  6, kind: TokenType.Word,       value: "In"             },
+      { min:  7, max:  7, kind: TokenType.Whitespace, value: " "              },
+      { min:  8, max:  9, kind: TokenType.Marker,     value: "\\w"            },
+      { min: 10, max: 10, kind: TokenType.Whitespace, value: " "              },
+      { min: 11, max: 13, kind: TokenType.Word,       value: "the"            },
+      { min: 14, max: 14, kind: TokenType.VBar,       value: "|"              },
+      { min: 15, max: 26, kind: TokenType.Word,       value: "lemma=\"test\"" },
+      { min: 27, max: 29, kind: TokenType.Marker,     value: "\\w*"           },
+      { min: 30, max: 38, kind: TokenType.Word,       value: "beginning"      },
     ]);
   });
 
   it("Real extracts", () => {
     // ASV
     let text = "\\v 44  \\f + \\fr 9:44  \\ft Vs. 44 and 46 (which are identical to v. 48) are omitted by some ancient authorities. \\fqa where their worm dieth not, and the fire is not quenched. \\f*";
+    let result = Array.from(tokenizer(text)).map((x) => {
+      delete x.min;
+      delete x.max;
+      return x;
+    });
 
-    expect(Array.from(tokenizer(text))).to.deep.equal([
+    expect(result).to.deep.equal([
       { kind: TokenType.Marker,     value: "\\v"            },
       { kind: TokenType.Whitespace, value: " "              },
       { kind: TokenType.Word,       value: "44"             },
