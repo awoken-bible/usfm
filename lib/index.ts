7127f2faef1a01399acf8796f1a6e1458fb865fe
Jamie Terry <jnterry@ntlworld.com>
2019-11-21 21:38:33 +0000

    Add index file to export public interface for package

---
+++ lib/index.ts
@@ -0,0 +1,19 @@
+import { readFile } from 'fs';
+
+import { parse, ParseResultBook } from './parser';
+
+export { parse };
+
+/**
+ * Yields a promise which eventually resolves to the parsed content for
+ * some file
+ */
+export function parseFile(path : string) : Promise<ParseResultBook>{
+	return new Promise((resolve, reject) => {
+		readFile(path, (err, data) => {
+			if(err){ return reject(err); }
+
+			return resolve(parse(data.toString()));
+		});
+	});
+}
